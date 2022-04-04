5f5e3dbb9cf8842cea7d6297bf5de29205c5cf2f
tube-jeonghoon <shu7081@gmail.com>
2022-04-04 20:37:55 +0000

    ArgoCD module init

---
+++ main.tf
@@ -0,0 +1,37 @@
+provider "kubernetes" {
+  # load_config_file = false
+  cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+  host                   = var.kubernetes_cluster_endpoint
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command     = "aws-iam-authenticator"
+    args        = ["token", "-i", "{var.kubernetes_cluster_name}"]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    # load_config_file = false
+    cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+    host                   = var.kubernetes_cluster_endpoint
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      command     = "aws-iam-authenticator"
+      args        = ["token", "-i", "{var.kubernetes_cluster_name}"]
+    }
+  }
+}
+
+# helm
+resource "kubernetes_namespace" "example" {
+  metadata {
+    name = "argo"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "msur"
+  chart      = "argo-cd"
+  repository = "https://argoproj.github.id/argo-helm"
+  namespace  = "argo"
+}
---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "kubernetes_cluster_id" {
+  type = string
+}
+
+variable "kubernetes_cluster_cert_data" {
+  type = string
+}
+
+variable "kubernetes_cluster_endpoint" {
+  type = string
+}
+
+variable "kubernetes_cluster_name" {
+  type = string
+}
+
+variable "eks_node_group_id" {
+  type = string
+}
+
